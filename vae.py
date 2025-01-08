0a21885e15d76418cdaca1d12aad6c8c85245719
disanda <skyanda1@qq.com>
2025-01-08 21:21:57 +0800

    vae_final

--- vae.py
+++ vae.py
@@ -6,8 +6,16 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 
-# 设置设备为 GPU id:0
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+import ssl
+ssl._create_default_https_context = ssl._create_stdlib_context #windows下载mnist出错需要设置
+
+import os
+folder_path = './vae_results' # 指定文件夹路径
+if not os.path.exists(folder_path): # 检查文件夹是否存在，如果不存在则创建
+    os.makedirs(folder_path)
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '1' # 设置GPU id 默认为0 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class VAE(nn.Module):
     def __init__(self, input_dim, latent_dim):
@@ -65,12 +73,12 @@ latent_dim = 32  # 潜在空间维度
 input_dim = 28 * 28  # MNIST 图像大小
 batch_size = 64
 epochs = 50
-lr = 0.001
+lr = 0.0005
 
 # 数据加载器
 #transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
 transform = transforms.Compose([transforms.ToTensor()])  # 只做 ToTensor 修改数据预处理，去除归一化到 [-1, 1]
-dataset = torchvision.datasets.MNIST(root='./data/mnist/', train=True, transform=transform, download=True)
+dataset = torchvision.datasets.MNIST(root='./data/mnist/', train=True, transform=transform)
 dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
 vae = VAE(input_dim=input_dim, latent_dim=latent_dim).to(device)
@@ -99,18 +107,18 @@ for epoch in range(epochs):
 
     print(f"Epoch [{epoch+1}/{epochs}] Loss: {total_loss / len(dataloader.dataset):.4f}")
 
-    if epoch%10 ==0:
+    if epoch%3 ==0:
         vae.eval()
         with torch.no_grad():
             # 从标准正态分布采样
             z = torch.randn(16*16, latent_dim).to(device)  # 16x16 = 256 个样本
-            generated_images = vae.decoder(z).view(-1, 1, 32, 32).cpu()
+            generated_images = vae.decoder(z).view(-1, 1, 28, 28).cpu()
 
             # 创建 16x16 的网格
             grid = torchvision.utils.make_grid(generated_images, nrow=16, normalize=True)
 
             # 保存生成的图像为文件
-            torchvision.utils.save_image(grid, './results/generated_images.png', normalize=True)
+            torchvision.utils.save_image(grid, './vae_results/generated_images_%d.png'%epoch, normalize=True)
 
             # 显示图像
             plt.imshow(grid.permute(1, 2, 0))
